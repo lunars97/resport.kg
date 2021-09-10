2e5c4ded7b8ed5c1193427d12e233a2a7755d955
Aizhan Alekbarova <alekbarova.aizhana@gmail.com>
2021-09-10 13:59:45 +0300

    make it better

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use("/products", productRoutes);
 app.use("/admin", productRoutes);
 app.use("/user", userRoute);
+
+app.get("/", (req, res) => {
+    res.send("App is running");
+});
 const PORT = process.env.PORT || 5000;
 
 mongoose
